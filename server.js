f5cee7e94427906f5da26957b3744fdea05265b5
anuragghsoh10 <anuragghosh1005@gmail.com>
2026-03-01 00:18:36 +0530

    Updated port for deployment

--- server.js
+++ server.js
@@ -86,4 +86,8 @@ app.post('/api/workorders', (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
