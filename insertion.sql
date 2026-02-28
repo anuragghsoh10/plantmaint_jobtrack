754e1d043f51efdd3e968b6c507a9febfd542b84
anuragghsoh10 <anuragghosh1005@gmail.com>
2026-03-01 00:07:29 +0530

    Add files via upload

---
+++ insertion.sql
@@ -0,0 +1,13 @@
+insert into users(username, password_hash, role) values('Anurag','$2b$10$XGFWXiLiP8dMNh6ej8v8VeTmNcs1ahW3YERWcXrFteo4tA5O9oAkm','olling Mill Operator');
+insert into users(username, password_hash, role) values('Nishant','$2b$10$Z1UkF5baFAWS.qfJIOCEl.OSa4gfIJ8TVk3WL7ywr.hkvmtxLqiwW','Shift in charge');
+insert into users(username, password_hash, role) values('Vaibhav','$2b$10$vfmm.VJKmGZBYyj93xOAg.OF04LRWXMYmEoC020iYnxmjjVJ7dE3u','Blast furnace operator');
+insert into users(username, password_hash, role) values('Yash','$2b$10$hmAFqoaatNGnRg8L7cYkKuJrFnVRIgMxo2SHLL15Dm0Knfmcbnsby','Stove Operator');
+insert into users(username, password_hash, role) values('Lassi','$2b$10$HHl4tF/iDs9HSuU7PIrJku3kWDn7C3B9PsJOi90UrYUT84PptRY2y','Plant Maintenance Supervisor');
+
+/*
+Anurag - Anurag@6535
+Nishant - nishant@200_04
+Vaibhav - vaibhavy@01a
+Yash - pandey#4567@91
+Lassi - lassi@#$%0102
+*/
